a6043cd92e46f857fb4b8c953af4dcec31b63937
Ade Attwood <code@adeattwood.co.uk>
2020-07-15 20:03:53 +0100

    refactor(commitlint): make commitlint testable and a little less biggie
    This refactors commitlint to use async / await not than `then` callback to
    control the out put and return code better. This also makes it better to await
    the whole action to make testing output better.

--- src/commands/commitlint.ts
+++ src/commands/commitlint.ts
@@ -14,6 +14,28 @@ const TYPE_WARNING = 1;
 const TYPE_ERROR = 2;
 
 /**
+ * The commit report object after it has been run through commitlint
+ */
+interface CommitReport {
+    /**
+     * If the commit is valid or not
+     */
+    valid: boolean;
+    /**
+     * An array of warnings in the commit message
+     */
+    warnings: any[];
+    /**
+     * An array of errors in the commit message
+     */
+    errors: any[];
+    /**
+     * The raw commit that was run through commitlint
+     */
+    input: string;
+}
+
+/**
  * Base config
  */
 const CONFIG: any = {
@@ -64,6 +86,23 @@ const CONFIG: any = {
     },
 };
 
+function lintCommits(params: any): Promise<Promise<CommitReport>[]> {
+    return new Promise(resolve => {
+        const promises: Promise<CommitReport>[] = [];
+        gitRawCommits(params)
+            .on('data', (commit: any) => {
+                promises.push(
+                    lint(
+                        commit.toString(),
+                        CONFIG.rules,
+                        CONFIG.parserPreset.parserOpts,
+                    ),
+                );
+            })
+            .on('end', () => resolve(promises));
+    });
+}
+
 export default class CommitLint extends Command {
     static description = 'Lint your commits against conventional commits';
 
@@ -92,60 +131,6 @@ export default class CommitLint extends Command {
             ];
         }
 
-        let exitCode = 0;
-        let commitCount = 0;
-        let finalCommitCount = -1;
-        let processedCount = 0;
-
-        const processCommit = (commit: string) => {
-            commitCount++;
-            lint(commit, CONFIG.rules, CONFIG.parserPreset.parserOpts)
-                .then((report: any) => {
-                    processedCount++;
-                    if (!report.errors.length && !report.warnings.length) {
-                        if (finalCommitCount === processedCount) {
-                            process.exit(exitCode);
-                        }
-                        return;
-                    }
-                    let errors = '';
-                    let warnings = '';
-                    if (flags.level > 0 && report.errors.length) {
-                        errors = report.errors
-                            .map(
-                                (item: any) =>
-                                    `${chalk.red('✖ ' + item.message)}`,
-                            )
-                            .join('\n');
-                    }
-
-                    if (flags.level > 1 && report.warnings.length) {
-                        warnings = report.warnings
-                            .map(
-                                (item: any) =>
-                                    `${chalk.yellow('⚠ ' + item.message)}`,
-                            )
-                            .join('\n');
-                    }
-
-                    if (errors.length || warnings.length) {
-                        process.stdout.write(
-                            `${report.input}\n${errors}\n${warnings}\n\n`,
-                        );
-                    }
-
-                    if (report.errors.length) {
-                        exitCode = 1;
-                    }
-
-                    if (finalCommitCount === processedCount) {
-                        process.exit(exitCode);
-                    }
-                })
-                .catch(() => {});
-        };
-
-        let editMsg = '';
         const root = await execa.command('git rev-parse --show-toplevel');
         if (fs.existsSync(`${root.stdout}/.git/COMMIT_EDITMSG`)) {
             const b = fs
@@ -157,34 +142,71 @@ export default class CommitLint extends Command {
                     '------------------------ >8 ------------------------',
                 ) || b.length;
 
-            editMsg = b
+            const editMsg = b
                 .substring(0, end)
                 .replace(/^#.*$/gm, '')
                 .trim();
 
-            processCommit(editMsg);
+            if (editMsg.length) {
+                const report = await lint(
+                    editMsg,
+                    CONFIG.rules,
+                    CONFIG.parserPreset.parserOpts,
+                );
+
+                if (report.errors.length) {
+                    this.log(`\n` + report.input);
+
+                    this.error(
+                        `Invalid commit message\n\n` +
+                            report.errors
+                                .map(
+                                    (item: any) =>
+                                        `${chalk.red('✖ ' + item.message)}`,
+                                )
+                                .join('\n'),
+                    );
+                }
+            }
         }
 
-        gitRawCommits({
-            from: flags.from,
-            to: 'HEAD',
-        })
-            .on('data', (rawCommit: any) => {
-                const commit = rawCommit.toString();
-                if (
-                    commit.trim() === 'Initial commit' ||
-                    commit.trim() === editMsg
-                ) {
-                    return;
-                }
+        let exitCode = 0;
+        const commits = await lintCommits({from: flags.from, to: 'HEAD'});
+        for await (const commit of commits) {
+            if (commit.input === 'Initial commit') {
+                continue;
+            }
 
-                processCommit(commit);
-            })
-            .on('end', () => {
-                finalCommitCount = commitCount;
-                if (finalCommitCount === processedCount) {
-                    process.exit(exitCode);
-                }
+            if (commit.errors.length > 0) {
+                exitCode = 1;
+            }
+
+            let errors = '';
+            let warnings = '';
+            if (flags.level > 0 && commit.errors.length) {
+                errors = commit.errors
+                    .map((item: any) => `${chalk.red('✖ ' + item.message)}`)
+                    .join('\n');
+            }
+
+            if (flags.level > 1 && commit.warnings.length) {
+                warnings = commit.warnings
+                    .map((item: any) => `${chalk.yellow('⚠ ' + item.message)}`)
+                    .join('\n');
+            }
+
+            if (errors.length || warnings.length) {
+                this.log(`${commit.input}\n${errors}\n${warnings}\n\n`);
+            }
+        }
+
+        if (exitCode > 0) {
+            this.error('Commitlint failed with errors', {
+                code: '2',
+                exit: false,
             });
+        }
+
+        this.exit(exitCode > 0 ? 2 : 0);
     }
 }
